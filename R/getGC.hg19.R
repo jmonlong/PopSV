829128cc2be3118cace83359e62161ec59172c4a
jmonlong <jean.monlong@mail.mcgill.ca>
2014-10-03 16:14:43 -0400

    remove bins with unknown GC content

--- R/getGC.hg19.R
+++ R/getGC.hg19.R
@@ -2,19 +2,19 @@
 ##' Requires BSgenome for Hsapiens hg19; if not installed instructions to install it will
 ##' be displayed.
 ##' @title GC content computation for specific bins
-##' @param bin.df a data.frame with bin information. Columns 'chr', 'start' and 'end'
+##' @param bins.df a data.frame with bin information. Columns 'chr', 'start' and 'end'
 ##' are required.
 ##' @return the same input data.frame with an extra column 'GCcontent'.
 ##' @author Jean Monlong
 ##' @export
-getGC.hg19 <- function(bin.df){
+getGC.hg19 <- function(bins.df){
     if(!require(BSgenome.Hsapiens.UCSC.hg19)){
         stop("Please install BSgenome first by running:
 > source(\"http://bioconductor.org/biocLite.R\")
 > biocLite(\"BSgenome.Hsapiens.UCSC.hg19\")
 ")
     }
-    bin.df$chunk = rep(1:ceiling(nrow(bin.df)/1e3),each=1e3)[1:nrow(bin.df)]
+    bins.df$chunk = rep(1:ceiling(nrow(bins.df)/1e3),each=1e3)[1:nrow(bins.df)]
     addGC <- function(df){
         if(!grepl("chr",df$chr[1])){
             chrs = paste("chr",df$chr,sep="")
@@ -24,9 +24,9 @@ getGC.hg19 <- function(bin.df){
         seq.l = Biostrings::getSeq(BSgenome.Hsapiens.UCSC.hg19::Hsapiens, chrs, df$start, df$end)
         lf = Biostrings::letterFrequency(seq.l,letters=c("G","C"))
         df$GCcontent = rowSums(lf) / (df$end-df$start)
-        df
+        subset(df, !is.na(GCcontent))
     }
-    bin.df = dplyr::do(dplyr::group_by(bin.df,chunk),addGC(.))
-    bin.df$chunk = NULL
-    return(bin.df)
+    bins.df = dplyr::do(dplyr::group_by(bins.df,chunk),addGC(.))
+    bins.df$chunk = NULL
+    return(bins.df)
 }
