d6665e4a48974b5bf6c477f975779b9b578b320b
jmonlong <jean.monlong@mail.mcgill.ca>
2014-10-29 13:01:33 -0400

    subset, outlier sample and export

--- R/qc.samples.summary.R
+++ R/qc.samples.summary.R
@@ -6,47 +6,69 @@
 ##' @author Jean Monlong
 ##' @export
 qc.samples.summary <- function(qc.res){
-        shiny::runApp(list(
-
+    shiny::runApp(list(
+        
         ui = shiny::fluidPage(
-            shiny::headerPanel("PopSV - Sample QC"),
+            shiny::titlePanel("PopSV - Sample QC"),
             shiny::sidebarPanel(
                 shiny::textInput("d", "D statistic threshold", "0.8"),
+                shiny::textInput("ols", "Outlier samples (',' separated)", ""),
+                shiny::conditionalPanel(condition = "input.conditionPanels == 'PCA'",
+                                        shiny::radioButtons("samp.lab", "Point : ",
+                                                            c("Point","Sample name"))),
+                shiny::conditionalPanel(condition = "input.conditionPanels != 'D distribution'",
+                                        shiny::radioButtons("samp.set", "Samples : ",
+                                                            c("All","Reference"))),
                 shiny::conditionalPanel(condition = "input.conditionPanels == 'Clustering'",
-                                 shiny::selectInput("cl.meth", "Linkage method: ",
-                                                    c("Average"="average","Complete"="complete","Ward"="ward.D")))
+                                        shiny::selectInput("cl.meth", "Linkage method: ",
+                                                           c("Average"="average","Complete"="complete","Ward"="ward.D"))),
+                hr(),
+                shiny::textOutput("nbsamp")
                 ),
             shiny::mainPanel(
                 shiny::tabsetPanel(
                     shiny::tabPanel("D distribution", shiny::plotOutput("d.hist")),
                     shiny::tabPanel("Clustering", shiny::plotOutput("clust")),
-                    shiny::tabPanel("PCA", shiny::plotOutput("pca")), id="conditionPanels"
+                    shiny::tabPanel("PCA", shiny::plotOutput("pca")),
+                    shiny::tabPanel("Export", shiny::actionButton("exp","Export to 'ref-samples.RData'"),hr(),shiny::textOutput("export")),id="conditionPanels"
                     )
-                )
-            ),
-
+                )),
+            
         server = function(input, output) {
             samples.ref <- shiny::reactive({
-                subset(qc.res$dstat, Dstat >= as.numeric(input$d))$sample
+                sr = as.character(subset(qc.res$dstat, Dstat >= as.numeric(input$d))$sample)
+                if(input$ols != ""){
+                    ols = unlist(strsplit(input$ols,","))
+                    return(setdiff(sr, ols))
+                } else {
+                    return(sr)
+                }
             })
             output$d.hist = shiny::renderPlot({
                 plot.df = qc.res$dstat
-                plot.df$reference = plot.df$sample %in% samples.ref()
+                plot.df$reference = factor(plot.df$sample %in% samples.ref(), levels=c("TRUE","FALSE"))
                 return(ggplot2::ggplot(plot.df, ggplot2::aes(x=Dstat)) + 
                        ggplot2::geom_histogram(ggplot2::aes(fill=reference), binwidth=.005) + 
+                       ggplot2::scale_fill_brewer(palette="Set1") +
                        ggplot2::theme(legend.position=c(0,1),legend.justification=c(0,1)) + 
-                       ggplot2::theme_bw() + ggplot2::ggtitle(paste(sum(plot.df$reference))) + 
+                       ggplot2::theme_bw() +
                        ggplot2::geom_vline(xintercept=as.numeric(input$d),linetype=2) +
                        ggplot2::ylab("number of samples"))
             })
             output$clust = shiny::renderPlot({
-                hc.o = hclust(as.dist(1-qc.res$cor.pw), method=input$cl.meth)
+                refs = samples.ref()
+                if(input$samp.set=="Reference"){
+                    hc.o = hclust(as.dist(1-qc.res$cor.pw[refs,refs]), method=input$cl.meth)
+                } else {
+                    hc.o = hclust(as.dist(1-qc.res$cor.pw), method=input$cl.meth)
+                }
                 dd <- ggdendro::dendro_data.hclust(hc.o)
                 l.df = dd$labels
-                l.df$reference = l.df$label %in% samples.ref()
+                l.df$reference = factor(l.df$label %in% refs, levels=c("TRUE","FALSE"))
                 return(ggplot2::ggplot(dd$segments) +
                        ggplot2::geom_segment(ggplot2::aes(x = x, y = y, xend = xend, yend = yend)) +
                        ggplot2::geom_point(ggplot2::aes(x=x, y=y, colour=reference),shape=18, size=5,data=l.df) +
+                       ggplot2::scale_colour_brewer(palette="Set1") +
                        ggplot2::theme_minimal() + ggplot2::theme(legend.position="bottom") +
                        ggplot2::scale_x_discrete(labels=l.df$label) +
                        ggplot2::xlab("sample") + ggplot2::ylab("") + 
@@ -55,12 +77,26 @@ qc.samples.summary <- function(qc.res){
             })
             output$pca = shiny::renderPlot({
                 plot.df = data.frame(sample=rownames(qc.res$pc.1.3), qc.res$pc.1.3, stringsAsFactors=FALSE)
-                plot.df$reference = plot.df$sample %in% samples.ref()
-                return(ggplot2::ggplot(plot.df, ggplot2::aes(x=PC1, y=PC2)) + 
-                       ggplot2::geom_point(ggplot2::aes(colour=reference)) + 
+                plot.df$reference = factor(plot.df$sample %in% samples.ref(), levels=c("TRUE","FALSE"))
+                if(input$samp.set=="Reference") plot.df = subset(plot.df, reference=="TRUE")
+                xlims = c(min(plot.df$PC1,na.rm=TRUE),max(plot.df$PC1,na.rm=TRUE))
+                xlims = c(xlims[1]-.07*diff(xlims),xlims[2]+.07*diff(xlims))
+                gpl = ggplot2::ggplot(plot.df, ggplot2::aes(x=PC1, y=PC2, colour=reference)) + 
                        ggplot2::theme(legend.position="bottom") + 
-                       ggplot2::theme_bw()
-                       )                
+                           ggplot2::scale_colour_brewer(palette="Set1") +
+                               ggplot2::theme_bw() + ggplot2::xlim(xlims[1],xlims[2])
+                if(input$samp.lab=="Point") return(gpl + ggplot2::geom_point(alpha=.7))
+                else return(gpl + ggplot2::geom_text(ggplot2::aes(label=sample)))
+            })
+            output$export = shiny::renderText({
+                input$exp
+                ref.samples = isolate(samples.ref())
+                save(ref.samples, file="ref-samples.RData")
+                return(paste(length(ref.samples)," samples saved in 'ref-samples.RData'."))
+            })
+            output$nbsamp = shiny::renderText({
+                ref.samples = samples.ref()
+                return(paste(length(ref.samples)," reference samples selected."))
             })
         }))
 
