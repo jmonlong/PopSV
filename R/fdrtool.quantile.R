ebe969afd5e8487d7d14ed7b7a4bd87664590f11
Jean Monlong <jean.monlong@mail.mcgill.ca>
2014-11-30 17:19:58 -0500

    remove z exactly 0

--- R/fdrtool.quantile.R
+++ R/fdrtool.quantile.R
@@ -14,7 +14,7 @@
 fdrtool.quantile <- function(z,quant.int = seq(.2,1,.02)){
     res = list(pval = rep(NA,length(z)),qval = rep(NA,length(z)), quant.int=NA, sigma.est=NA)
     z[which(is.infinite(z))] = NA ## Remove infinite values
-    non.na.i = which(!is.na(z))
+    non.na.i = which(!is.na(z) & z!=0)
     z.non.na = z[non.na.i]
     sigma.est = sapply(quant.int, function(qi)fdrtool::censored.fit(z.non.na,quantile(abs(z.non.na),probs=qi,na.rm=TRUE))[5])
     res$quant.int = quant.int[which.min(abs(diff(sigma.est)))]
