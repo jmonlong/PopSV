8a684aa30b9dd8b14954fcfbdff3afa72117499a
Jean Monlong <jean.monlong@mail.mcgill.ca>
2014-12-18 23:09:17 -0500

    dev other norm

--- R/medvar.norm.R
+++ R/medvar.norm.R
@@ -4,13 +4,47 @@
 ##' @return a matrix with the normalized bin counts (bin x sample).
 ##' @author Jean Monlong
 ##' @export
-medvar.norm <- function(bc){
-    med = apply(bc, 2, median, na.rm=TRUE)
+medvar.norm <- function(bc, ref.samples, bc.support=NULL, z.poisson=FALSE){
+    all.samples = setdiff(colnames(bc),c("chr","start","end"))
+    ref.samples.ii = which(all.samples %in% ref.samples)
+    rownames(bc) = bins = paste(bc$chr, as.integer(bc$start), sep="-")
+    norm.stats = createEmptyDF(c("character", rep("integer",2),rep("numeric",3)), length(bins))
+    colnames(norm.stats) = c("chr", "start","end","m","sd","nb.remove")
+    bc.norm = createEmptyDF(c("character", rep("integer",2),rep("numeric",length(all.samples))), length(bins))
+    z = createEmptyDF(c("character", rep("integer",2),rep("numeric",length(all.samples))), length(bins))
+    fc = createEmptyDF(c("character", rep("integer",2),rep("numeric",length(all.samples))), length(bins))
+    colnames(bc.norm) = colnames(z) = colnames(fc) = c("chr", "start","end",all.samples)
+    norm.stats$chr = bc.norm$chr = z$chr = fc$chr = bc[bins,"chr"]
+    norm.stats$start = bc.norm$start = z$start = fc$start = bc[bins,"start"]
+    norm.stats$end = bc.norm$end = z$end = fc$end = bc[bins,"end"]
+    if(z.poisson){
+        z.comp <- function(x, mean.c, sd.c){
+            z.n = (x-mean.c)/sd.c
+            z.p = qnorm(ppois(x, mean.c))
+            n.ii = abs(z.n) < abs(z.p)
+            z.p[n.ii] = z.n[n.ii]
+            z.p
+        }
+    } else {
+        z.comp <- function(x, mean.c, sd.c){(x-mean.c)/sd.c}
+    }
+
+    if(is.null(bc.support)){
+        bc.support = bc[,all.samples]
+    } else {
+        bc.support = bc.support[,all.samples]
+    }
+    med = apply(bc.support, 2, median, na.rm=TRUE)
     med.c = mean(med)
-    bc = t(t(bc) * med.c / med)
-    bc = bc - med.c
-    md = apply(bc,2,function(x)median(abs(x),na.rm=TRUE))
-    md.c = median(abs(bc),na.rm=TRUE)
-    bc = t(t(bc) * md.c / md)
-    return(bc + med.c)
+    bc.support = t(t(bc.support) * med.c / med)
+    bc.support = bc.support - med.c
+    md = apply(bc.support,2,function(x)median(abs(x),na.rm=TRUE))
+    md.c = median(abs(bc.support),na.rm=TRUE)
+    bc.norm[,-(1:3)] = t(t(bc[,all.samples]) * md.c / md) + med.c
+    msd = apply(bc.norm[,ref.samples], 1, function(ee)unlist(mean.sd.outlierR(ee), pv.max.ol=1e-6))
+    norm.stats[,4:6]=cbind(msd[1,],msd[2,],msd[3,])
+    z[,-(1:3)] = apply(bc.norm[,-(1:3)], 2, z.comp,mean.c=norm.stats$m,sd.c=norm.stats$sd)
+    fc[,-(1:3)] = bc.norm[,-(1:3)]/norm.stats$m
+    
+    list(norm.stats=norm.stats, bc.norm=bc.norm,z=z, fc=fc, z.poisson=z.poisson)
 }
