ed6c001bb78472402ad9bcb1620781332edbde8d
jmonlong <jean.monlong@mail.mcgill.ca>
2015-01-23 11:28:40 -0500

    winsor visibility pb

--- R/winsor.R
+++ R/winsor.R
@@ -7,7 +7,11 @@
 ##' @author Jean Monlong
 ##' @keywords internal
 winsor <- function(x, u=5, l=NULL){
-  if(!is.null(u) & any(x>u)) x[x>u] = u
-  if(!is.null(l) & any(x>l)) x[x<l] = l
-  x
+  if(!is.null(u) & any(x>u)){
+    x[x>u] = u
+  }
+  if(!is.null(l) & any(x>l)){
+    x[x<l] = l
+  }
+  return(x)
 }
