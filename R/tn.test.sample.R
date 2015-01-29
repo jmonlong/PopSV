6ab93b656d0ad6d88bc14b922bda09ce52c7fd9f
jmonlong <jean.monlong@mail.mcgill.ca>
2015-01-29 16:40:17 -0500

    debug test single sample

--- R/tn.test.sample.R
+++ R/tn.test.sample.R
@@ -56,29 +56,28 @@ tn.test.sample <- function(test.sample, cont.sample, files.df, norm.stat.f, z.po
     res.df$end = test.bc$end
 
     test.bin <- function(ns){
-        bin = paste(ns[1],as.integer(ns[2]), sep="-")
-        if(aberrant.cases){
-            norm.coeff = norm.tm.opt(test.bc[id.test[ns[-(1:7)]],"bc",drop=FALSE],ref.col=cont.bc[id.cont[ns[-(1:7)]],"bc"],bc.mean.norm=as.numeric(ns[4]),chrs=test.bc[id.test[ns[-(1:7)]],"chr"])
-            bc.n = test.bc[id.test[bin],"bc"] * norm.coeff
-            return(c(bc = bc.n,
-                     z = z.comp(bc.n,as.numeric(ns[4]),as.numeric(ns[5])),
-                     fc = bc.n/as.numeric(ns[4])))
-        } else {
-            norm.coeff = norm.tm.opt(test.bc[id.test[ns[-(1:7)]],"bc",drop=FALSE],ref.col=cont.bc[id.cont[ns[-(1:7)]],"bc"])
-            bc.n = test.bc[id.test[bin],"bc"] * norm.coeff
-            return(c(bc = bc.n,
-                     z = z.comp(bc.n,as.numeric(ns[4]),as.numeric(ns[5])),
-                     fc = bc.n/as.numeric(ns[4])))
-        }
+      bin = paste(ns[1],as.integer(ns[2]), sep="-")
+      if(aberrant.cases){
+        norm.coeff = norm.tm.opt(test.bc[id.test[ns[-(1:7)]],"bc",drop=FALSE],ref.col=cont.bc[id.cont[ns[-(1:7)]],"bc"],bc.mean.norm=as.numeric(ns[4]),chrs=test.bc[id.test[ns[-(1:7)]],"chr"])
+      } else {
+        norm.coeff = norm.tm.opt(test.bc[id.test[ns[-(1:7)]],"bc",drop=FALSE],ref.col=cont.bc[id.cont[ns[-(1:7)]],"bc"])
+      }
+      bc.n = test.bc[id.test[bin],"bc"] * norm.coeff
+      return(c(bc = bc.n,
+               z = z.comp(bc.n,as.numeric(ns[5]),as.numeric(ns[6])),
+               fc = bc.n/as.numeric(ns[5])))
     }
-
+    
     con = file(norm.stat.f,"r")
     headers = unlist(strsplit(readLines(con,n=1),"\t"))
     while(length((lines = readLines(con,n=chunk.size)))>0){
         norm.chunk = matrix(unlist(strsplit(lines, "\t")), ncol=length(headers), byrow=TRUE)
         colnames(norm.chunk) = headers
         norm.chunk = norm.chunk[norm.chunk[,4]!="NA",]
-        res.df[id.test[norm.chunk[,8]], 4:6] = t(apply(norm.chunk, 1, test.bin))
+        bins = paste(norm.chunk[,1],as.integer(norm.chunk[,2]), sep="-")
+        res.df[id.test[bins], 4:6] = t(apply(norm.chunk, 1, test.bin))
     }
+    close(con)
+
     return(res.df)
 }
