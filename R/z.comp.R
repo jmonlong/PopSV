2e27ab1c9a68a28c3fdfeff83bb6dcedec44aafb
Jean Monlong <jean.monlong@mail.mcgill.ca>
2015-02-12 19:29:15 -0500

    opt

--- R/z.comp.R
+++ R/z.comp.R
@@ -25,34 +25,22 @@ z.comp <- function(files.df, samples, z.poisson=FALSE, col="bc.gc.tnk", nb.cores
     z.comp.f <- function(x, mean.c, sd.c){(x-mean.c)/sd.c}
   }
   bc.1 = read.table(subset(files.df, sample==samples[1])[,col],header=TRUE, as.is=TRUE)
-  bc.df = createEmptyDF(c("character",rep("integer",2), rep("numeric",length(samples))), nrow(bc.1))
-  z.df = createEmptyDF(c("character",rep("integer",2), rep("numeric",length(samples))), nrow(bc.1))
-  fc.df = createEmptyDF(c("character",rep("integer",2), rep("numeric",length(samples))), nrow(bc.1))
-  msd.df = createEmptyDF(c("character",rep("integer",2), rep("numeric",3)), nrow(bc.1))
-  colnames(bc.df) = colnames(z.df) = colnames(fc.df) = c("chr","start","end", samples)
-  colnames(msd.df) = c("chr","start","end", "m","sd","nb.remove")
-  bc.df$chr = z.df$chr = fc.df$chr = bc.df$chr
-  bc.df$start = z.df$start = fc.df$start = bc.df$start
-  bc.df$end = z.df$end = fc.df$end = bc.df$end
-  bc.df[,samples[1]] = bc.1[,4]
   if(nb.cores>1){
-    bc.l = parallel::mclapply(subset(files.df, sample%in%samples[-1])[,col], function(fi){
+    bc.l = parallel::mclapply(subset(files.df, sample%in%samples)[,col], function(fi){
       read.table(fi,header=TRUE, as.is=TRUE)[,4]
     },mc.cores=nb.cores)
   } else {
-    bc.l = lapply(subset(files.df, sample%in%samples[-1])[,col], function(fi){
+    bc.l = lapply(subset(files.df, sample%in%samples)[,col], function(fi){
       read.table(fi,header=TRUE, as.is=TRUE)[,4]
     })
   }
-  for(samp.i in 1:(length(samples)-1)){
-    bc.df[,as.character(samples[1+samp.i])] = bc.l[[samp.i]]
-  }
-  for(ii in 1:nrow(bc.df)){
-    bc.t = bc.df[ii,-(1:3)]
-    msd = mean.sd.outlierR(bc.t,1e-6)
-    msd.df[ii, -(1:3)] = with(msd, c(m, sd, nb.remove))
-    z.df[ii,-(1:3)] = z.comp.f(bc.t,msd$m,msd$sd)
-    fc.df[ii,-(1:3)] = bc.t/msd$m
-  }
-  return(list(z=z.df, fc=fc.df, msd=msd.df, z.poisson=z.poisson))
+  bc.l = matrix(unlist(bc.l), length(bc.l[[1]]))
+  msd = apply(bc.l, 1, function(ee)unlist(mean.sd.outlierR(ee, pv.max.ol=1e-6)))
+  z = apply(bc.l, 2, z.comp.f, mean.c=msd[1,], sd.c=msd[2,])
+  fc = bc.l/msd[1,]
+  colnames(z) = colnames(fc) = samples
+  z = data.frame(bc.1[,1:3],z)
+  fc = data.frame(bc.1[,1:3],fc)
+  msd = data.frame(bc.1[,1:3],t(msd))
+  return(list(z=z, fc=fc, msd=msd, z.poisson=z.poisson))
 }
