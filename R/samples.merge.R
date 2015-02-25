345981e471925f72741bedfdad62e6b671b3a13d
jmonlong <jean.monlong@mail.mcgill.ca>
2015-02-25 15:26:28 -0500

    fread hanndling

--- R/samples.merge.R
+++ R/samples.merge.R
@@ -4,17 +4,18 @@
 ##' @param samples the names of the samples to use.
 ##' @param files.col the name of 'files.df' to use for each 'res' element.
 ##' @param nb.cores the number of cores to use.
+##' @param sig.digits the number of significant digits. Default is 6.
 ##' @return the merged data.frame
 ##' @author Jean Monlong
 ##' @export
-samples.merge <- function(files.df, samples, files.col, nb.cores=1){
+samples.merge <- function(files.df, samples, files.col, nb.cores=1, sig.digits=6){
   
-  res.1 = data.table::fread(subset(files.df, sample==samples[1])[,files.col],header=TRUE)
+  res.1 = as.data.frame(data.table::fread(subset(files.df, sample==samples[1])[,files.col],header=TRUE))
   res.l = parallel::mclapply(subset(files.df, sample%in%samples)[,files.col], function(fi){
     unlist(data.table::fread(fi,header=TRUE)[,4, with=FALSE])
   },mc.cores=nb.cores)
-  res.l = matrix(unlist(res.l), length(res.l[[1]]))
+  res.l = matrix(signif(unlist(res.l),sig.digits), length(res.l[[1]]))
   colnames(res.l) = samples
   
-  return(cbind(res.1[,c("chr","start","end")],res.l))
+  return(cbind(res.1[,c("chr","start","end"), with=FALSE],res.l))
 }
