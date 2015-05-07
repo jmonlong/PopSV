086b6148e6595cf2345b305bd638621595b4392c
jmonlong <jean.monlong@mail.mcgill.ca>
2015-05-07 16:17:55 -0400

    sliding window parameter

--- R/fragment.genome.hg19.R
+++ R/fragment.genome.hg19.R
@@ -2,19 +2,19 @@
 ##' require package \code{BSgenome.Hsapiens.UCSC.hg19} to be installed.
 ##' @title Fragment hg19 genome
 ##' @param bin.size the size of the bins
+##' @param slid.window the size of the sliding window. Default is the size of the bin, i.e. not overlapping windows.
 ##' @return a data.frame with columns 'chr', 'start' and 'end'.
 ##' @author Jean Monlong
 ##' @import GenomicRanges
 ##' @import GenomeInfoDb
 ##' @export
-fragment.genome.hp19 <- function(bin.size = 1000) {
+fragment.genome.hp19 <- function(bin.size = 1000, slid.window=bin.size) {
     if (!require(BSgenome.Hsapiens.UCSC.hg19, quietly = TRUE)) {
         stop("Please install BSgenome first by running:\n> source(\"http://bioconductor.org/biocLite.R\")\n> biocLite(\"BSgenome.Hsapiens.UCSC.hg19\")\n")
     }
-    seql.1.22 = seqlengths(BSgenome.Hsapiens.UCSC.hg19::BSgenome.Hsapiens.UCSC.hg19)[paste0("chr", 
-        1:22)]
+    seql.1.22 = seqlengths(BSgenome.Hsapiens.UCSC.hg19::BSgenome.Hsapiens.UCSC.hg19)[paste0("chr", 1:22)]
     fragment.chr <- function(chr.i) {
-        starts = as.integer(seq(1, seql.1.22[chr.i], bin.size))
+        starts = as.integer(seq(1, seql.1.22[chr.i], slid.window))
         ends = as.integer(starts + bin.size - 1)
         ends[length(ends)] = as.integer(seql.1.22[chr.i])
         data.frame(chr = chr.i, start = starts, end = ends)
