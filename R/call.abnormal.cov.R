5c39c05f72dfd1f816688281c05580a0199627d2
Jean Monlong <jean.monlong@mail.mcgill.ca>
2015-05-19 18:07:23 -0400

    catch error when no call at all...

--- R/call.abnormal.cov.R
+++ R/call.abnormal.cov.R
@@ -185,9 +185,12 @@ call.abnormal.cov <- function(z=NULL, files.df=NULL, samp, out.pdf = NULL, FDR.t
   ## Deviation from copy number 2
   res.df$cn2.dev = 2*abs(res.df$fc - 1)
   res.df$cn = round(2*res.df$fc)
-  res.df$prop.single.bin = mean(res.df$nb.bin.cons==1)
-  
-  
+  if(nrow(res.df)>0){
+    res.df$prop.single.bin = mean(res.df$nb.bin.cons==1)
+  } else {
+    res.df$prop.single.bin = numeric(0)
+  }
+    
   if (nrow(res.df) > 0 & merge.cons.bins[1] != "no") {
     return(data.frame(sample = samp, res.df, stringsAsFactors = FALSE))
   } else if (any(res.df$qv <= FDR.th, na.rm = TRUE)) {
