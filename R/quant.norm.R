a6968015b7aa17f4f44d38c9d938bd4698ba072e
jmonlong <jean.monlong@mail.mcgill.ca>
2015-05-22 15:15:46 -0400

    optimize a bit

--- R/quant.norm.R
+++ R/quant.norm.R
@@ -11,27 +11,21 @@
 ##' @export
 quant.norm <- function(bc.df, nb.cores = 1, norm.stats.comp = TRUE) {
     all.samples = setdiff(colnames(bc.df), c("chr", "start", "end"))
-    rownames(bc.df) = bins = paste(bc.df$chr, as.integer(bc.df$start), as.integer(bc.df$end), 
-        sep = "-")
+    rownames(bc.df) = bins = paste(bc.df$chr, as.integer(bc.df$start), as.integer(bc.df$end), sep = "-")
     
-    norm.stats = createEmptyDF(c("character", rep("integer", 2), rep("numeric", 3)), 
-        length(bins))
+    norm.stats = createEmptyDF(c("character", rep("integer", 2), rep("numeric", 3)), length(bins))
     colnames(norm.stats) = c("chr", "start", "end", "m", "sd", "nb.remove")
-    bc.norm = createEmptyDF(c("character", rep("integer", 2), rep("numeric", length(all.samples))), 
-        length(bins))
+    bc.norm = createEmptyDF(c("character", rep("integer", 2), rep("numeric", length(all.samples))), length(bins))
     colnames(bc.norm) = c("chr", "start", "end", all.samples)
     norm.stats$chr = bc.norm$chr = bc.df$chr
     norm.stats$start = bc.norm$start = bc.df$start
     norm.stats$end = bc.norm$end = bc.df$end
     
     bc.mat = as.matrix(bc.df[, all.samples])
-    bc.cov = as.numeric(parallel::mclapply(1:ncol(bc.mat), function(cc) median(bc.mat[, 
-        cc], na.rm = TRUE), mc.cores = nb.cores))
+    bc.cov = as.numeric(parallel::mclapply(1:ncol(bc.mat), function(cc) median(bc.mat[, cc], na.rm = TRUE), mc.cores = nb.cores))
     bc.mat = (bc.mat * median(bc.cov)) %*% diag(1/bc.cov)
-    bc.sorted = matrix(as.numeric(unlist(parallel::mclapply(1:ncol(bc.mat), function(cc) sort(bc.mat[, 
-        cc]), mc.cores = nb.cores))), nrow(bc.mat))
-    med = round(as.numeric(parallel::mclapply(1:nrow(bc.sorted), function(rr) median(bc.sorted[rr, 
-        ], na.rm = TRUE), mc.cores = nb.cores)), 2)
+    bc.sorted = matrix(as.numeric(unlist(parallel::mclapply(sample(1:ncol(bc.mat),5), function(cc) sort(bc.mat[, cc]), mc.cores = nb.cores))), nrow(bc.mat))
+    med = round(as.numeric(parallel::mclapply(1:nrow(bc.sorted), function(rr) median(bc.sorted[rr, ], na.rm = TRUE), mc.cores = nb.cores)), 2)
     bc.norm[, -(1:3)] = matrix(as.numeric(unlist(parallel::mclapply(1:ncol(bc.mat), 
         function(cc) med[rank(bc.mat[, cc])], mc.cores = nb.cores))), nrow(bc.mat))
     
