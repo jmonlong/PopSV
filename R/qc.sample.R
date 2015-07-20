7a40dec1ba850838508d34bf6bf9c9456e6651d1
jmonlong <jean.monlong@mail.mcgill.ca>
2015-07-20 13:01:48 -0400

    add low coverage qc metrics

--- R/qc.sample.R
+++ R/qc.sample.R
@@ -33,6 +33,16 @@ qc.sample <- function(bins.df, files.df=NULL, cnv.df=NULL, ref.samples=NULL, n.s
     ## d.o = rowMeans(d.geom[,ref.samples])/mean(d.geom[,ref.samples])
     d.o = apply(d.geom[,ref.samples], 1, function(d.f) mean(d.f[d.f<quantile(d.f, probs=.1, na.rm=TRUE)], na.rm=TRUE)) / mean(d.geom[d.geom<quantile(d.geom, probs=.1, na.rm=TRUE)], na.rm=TRUE)
     res = data.frame(sample=samples, dist.bc.ref=d.o)
+
+    if(any(colnames(bins.df)=="m")){
+      lowcov.bins = bins.df[head(order(bins.df$m),n.subset),]
+      bc.df = quick.count(files.df, bins.df, col.files="bc.gc.gz", nb.rand.bins=n.subset, nb.cores=nb.cores)
+      bc.df = med.norm(bc.df, nb.cores=nb.cores, norm.stats.comp=FALSE)$bc.norm
+      d.geom = as.matrix(dist(t(bc.df[,samples])))
+      colnames(d.geom) = samples
+      d.o = rowMeans(d.geom[,ref.samples])/mean(d.geom[,ref.samples])
+      res$dist.bc.ref.lowcov = d.o
+    }
   }
 
   if(!is.null(cnv.df)){
