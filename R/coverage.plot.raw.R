3c025790984cba059d4ebd8945654b2d1e386269
jmonlong <jean.monlong@mail.mcgill.ca>
2015-09-24 12:07:42 -0400

    new visualization

--- R/coverage.plot.raw.R
+++ R/coverage.plot.raw.R
@@ -1,15 +1,18 @@
 ##' Plot the raw read coverage in a genomic regions across samples.
 ##' @title  Plot the raw coverage in a region
-##' @param gr a 'GRanges' object corresponding to the region of interest
-##' @param files.df the data.frame with the path to the bam files for each sample
+##' @param chr chromosome 
+##' @param start genomic coordinate
+##' @param end genomic coordinate
+##' @param files.df a data.frame with the path to the bam files
 ##' @param samples the samples to represent
 ##' @param proper should proper mapping be counted. Default is TRUE.
 ##' @param map.quality the minimum mapping quality. Default is 30.
 ##' @param bp.res the resolution, in bp. Default is 1 bp.
+##' @param flanks the amount of bp to add in the flanks.
 ##' @return a ggplot object
 ##' @author Jean Monlong
 ##' @export
-coverage.plot.raw <- function(gr, files.df, samples, proper=TRUE, map.quality=30, bp.res=1, flanks=NULL){
+coverage.plot.raw <- function(chr, start, end, files.df, samples, proper=TRUE, map.quality=30, bp.res=1, flanks=NULL){
 
   gr.bam <- function(bam.file, gr, proper = TRUE, map.quality = 30) {
     bai.file = sub("bam$", "bai", bam.file, perl = TRUE)
@@ -27,46 +30,46 @@ coverage.plot.raw <- function(gr, files.df, samples, proper=TRUE, map.quality=30
     if(is.null(bam.df)) return(GenomicRanges::GRanges())
     return(with(bam.df, GenomicRanges::GRanges(rname, IRanges::IRanges(pos, width = qwidth), bin = bin)))
   }
-  cov.gr <- function(gr.i,bin.size=10){
-    gr.bk = seq(min(start(gr.i)), max(end(gr.i)), bin.size)
-    gr.cov = GRanges(seqnames(gr.i)[1], IRanges(gr.bk[-length(gr.bk)], width=bin.size))
-    cov.t = table(cut((start(gr.i)+end(gr.i))/2, gr.bk, labels=FALSE))
-    gr.cov$cov = 0
-    gr.cov$cov[as.integer(names(cov.t))] = as.integer(cov.t)
-    gr.cov
+  cov.reads <- function(reads.gr, win.gr){
+    GenomicRanges::countOverlaps(win.gr, reads.gr)
   }
-
+  
   if(!is.null(flanks)){
-    bkpts = c(start(gr), end(gr))
-    start(gr) = start(gr)-flanks
-    end(gr) = end(gr)+flanks
+    bkpts = c(start, end)
+    start = start-flanks
+    end = end+flanks
   }
 
-  if(bp.res>1){
-    gr.l = lapply(samples, function(samp.i){
-                    cov.gr = cov.gr(gr.bam(files.df$bam[files.df$sample==samp.i], gr, proper=proper, map.quality=map.quality), bp.res)
-                    cov.gr$sample = samp.i
-                    cov.gr
-                  })
-    gr.ul = GRanges()
-    for(ii in 1:length(gr.l)) gr.ul = c(gr.ul, gr.l[[ii]])
-    final.p = ggbio::autoplot(gr.ul, geom="area",ggplot2::aes(y=cov),facets=sample~.) + ggplot2::theme_bw() + ggplot2::facet_grid(sample~.,scales="free")
+  gr.f = GenomicRanges::GRanges(chr, IRanges::IRanges(start, end))
+  reads.l = lapply(samples, function(samp.i){
+    gr.bam(files.df$bam[files.df$sample==samp.i], gr.f, proper=proper, map.quality=map.quality)
+  })
+
+  gr.bk = seq(GenomicRanges::start(gr.f), GenomicRanges::end(gr.f), bp.res)
+  gr.frag = GenomicRanges::GRanges(chr, IRanges::IRanges(gr.bk[-length(gr.bk)], width = bp.res))
+  cov.l = lapply(1:length(reads.l), function(ii) {
+    cov.reads(reads.l[[ii]], gr.frag)
+  })
+  cov = matrix(0, length(gr.frag), length(samples))
+  for (ii in 1:length(samples)) cov[, ii] = cov.l[[ii]]
+  if(!is.null(flanks)){
+    norm.f = apply(cov[c(1:floor(flanks/bp.res),(nrow(cov)-floor(flanks/bp.res)+1):nrow(cov)),],2,mean, na.rm=TRUE)
   } else {
-    gr.l = lapply(samples, function(samp.i){
-                    reads.gr = gr.bam(files.df$bam[files.df$sample==samp.i], gr, proper=proper, map.quality=map.quality)
-                    reads.gr$sample = samp.i
-                    reads.gr
-                  })
-    ##gr.l = unlist(GenomicRanges::GRangesList(gr.l))
-    gr.ul = GRanges()
-    for(ii in 1:length(gr.l)) gr.ul = c(gr.ul, gr.l[[ii]])
-    gr.ul$sample = factor(gr.ul$sample, levels=samples)
-    final.p = ggbio::autoplot(gr.ul, facets=sample~.,stat="coverage") + ggplot2::theme_bw() + ggplot2::facet_grid(sample~.,scales="free")
+    norm.f = rep(1,length(samples))
   }
-
+  cov = cov %*% diag(mean(norm.f)/norm.f)
+  colnames(cov) = samples
+  rownames(cov) = GenomicRanges::start(gr.frag)
+  cov.df = reshape::melt(cov)
+  colnames(cov.df) = c("position","sample","cov")
+  cov.df$sample = factor(cov.df$sample, levels=samples)
+  cov.df$abnormal = cov.df$sample==samples[1]
+  
+  final.p = ggplot2::ggplot(subset(cov.df,!abnormal), ggplot2::aes(x=position,y=cov, group=sample)) + ggplot2::geom_line(alpha=.8) + ggplot2::geom_line(data=subset(cov.df,abnormal), size=3) + ggplot2::theme_bw() + ggplot2::scale_size_manual(values=c(1,2))
   if(!is.null(flanks)){
     final.p = final.p + ggplot2::geom_vline(xintercept=bkpts,linetype=2)
   }
-
+  final.p
+  
   return(final.p)
 }
