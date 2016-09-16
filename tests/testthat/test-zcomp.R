c8a8b9fe93a73f57b0fdbf1864e9cd6a031c2dce
jmonlong <jean.monlong@mail.mcgill.ca>
2016-09-16 18:19:32 -0400

    update test after simplification

--- tests/testthat/test-zcomp.R
+++ tests/testthat/test-zcomp.R
@@ -1,51 +1,42 @@
 context("Z-score computation")
 
-## Create fake bins and bin counts
+## Create fake bins, bin counts and norm stats
 bc.df = data.frame(chr=1, start=seq(1,1e4,10))
 bc.df$end = bc.df$start + 9
 bc.mat = replicate(10, rnorm(nrow(bc.df), 100, 10))
 colnames(bc.mat) = paste0("s",1:10)
+bc.mat = bc.mat[sample.int(nrow(bc.mat)),]
 
 ## Write files
 files.df = data.frame(sample=colnames(bc.mat), stringsAsFactors=FALSE)
-files.df$bc.gc.norm.gz = paste0(files.df$sample, "-test-bc.tsv")
 files.df$z = paste0(files.df$sample, "-test-z.tsv")
 files.df$fc = paste0(files.df$sample, "-test-fc.tsv")
-dump = sapply(1:10, function(ii){
-                  bc.df$bc = bc.mat[,ii]
-                  write.table(bc.df, file=files.df$bc.gc.norm.gz[ii], sep="\t", row.names=FALSE, col.names=TRUE)
-              })
 
 bc.f = "tempbc.tsv"
+ns.f = "tempns.tsv"
 bc.df = cbind(bc.df, bc.mat)
+ns.df = bc.df[,1:3]
+ns.df$m = rnorm(nrow(bc.df), 100, 10)
+ns.df$sd = abs(rnorm(nrow(bc.df), 10, 10))
 write.table(bc.df, file=bc.f, sep="\t", row.names=FALSE, col.names=TRUE)
+write.table(ns.df, file=ns.f, sep="\t", row.names=FALSE, col.names=TRUE)
 
 test_that("It runs with different parameters",{
-              z.df = z.comp(bc.f, files.df)
+              z.df = z.comp(bc.f, ns.f, files.df)
               expect_true(all(file.exists(paste0(files.df$z, ".bgz"))))
               expect_true(all(file.exists(paste0(files.df$fc, ".bgz"))))
-              expect_true(file.exists("ref-msd.tsv"))
-              z.df = z.comp(bc.f, files.df, z.poisson=TRUE)
+              z.df = z.comp(bc.f, ns.f, files.df, z.poisson=TRUE)
               expect_true(all(file.exists(paste0(files.df$z, ".bgz"))))
               expect_true(all(file.exists(paste0(files.df$fc, ".bgz"))))
-              expect_true(file.exists("ref-msd.tsv"))
-              z.df = z.comp(bc.f, files.df, ref.samples=head(files.df$sample))
+              z.df = z.comp(bc.f, ns.f, files.df, chunk.size=50)
               expect_true(all(file.exists(paste0(files.df$z, ".bgz"))))
               expect_true(all(file.exists(paste0(files.df$fc, ".bgz"))))
-              expect_true(file.exists("ref-msd.tsv"))
-              z.df = z.comp(files.df=files.df)
-              expect_true(all(file.exists(paste0(files.df$z, ".bgz"))))
-              expect_true(all(file.exists(paste0(files.df$fc, ".bgz"))))
-              expect_true(file.exists("ref-msd.tsv"))
-              z.df = z.comp(bc.f, files.df=files.df, chunk.size=100)
-          })
+})
 
 test_that("Bad input raise errors",{
-              expect_error(z.comp("badpath", files.df), "file not found")
-              expect_error(z.comp(files.df=files.df, files.col=NULL), "non-NULL")
-              expect_error(z.comp(bc.f, files.df=files.df, ref.samples = c("badsample",files.df$sample)), "not present")
-          })
+  expect_error(z.comp("badpath", files.df), "file not found")
+})
 
 
 ## Remove files
-dump = file.remove(files.df$bc.gc.norm.gz, paste0(files.df$z, ".bgz"), paste0(files.df$z, ".bgz.tbi"), paste0(files.df$fc, ".bgz"), paste0(files.df$fc, ".bgz.tbi"), bc.f, "ref-msd.tsv")
+dump = file.remove(paste0(files.df$z, ".bgz"), paste0(files.df$z, ".bgz.tbi"), paste0(files.df$fc, ".bgz"), paste0(files.df$fc, ".bgz.tbi"), bc.f, ns.f)
