bf28043c75ea81575c0fb45fdd3dc66f9ba83889
Jean Monlong <jean.monlong@mail.mcgill.ca>
2016-09-24 18:37:55 -0400

    [ci skip] more features

--- R/qc.samples.cluster.R
+++ R/qc.samples.cluster.R
@@ -4,10 +4,11 @@
 ##' @title QC sample : clustering
 ##' @param bc.df a data.frame with the bin counts.
 ##' @param nb.rand.bins if non-NULL, the number of bins to randomly choose. Default is NULL.
-##' @return a shiny app in the web browser
+##' @param shiny.app If TRUE (default), a shiny app is launched in a web browser. If FALSE, the function simply returns the PCA results.
+##' @return a shiny app in the web browser, or a data.frame with the results of the PCA (if shiny.app=FALSE).
 ##' @author Jean Monlong
 ##' @export
-qc.samples.cluster <- function(bc.df, nb.rand.bins=NULL){
+qc.samples.cluster <- function(bc.df, nb.rand.bins=NULL, shiny.app=TRUE){
   if(!is.null(nb.rand.bins)){
     bins.df = bins.df[sample.int(nrow(bins.df), min(nrow(bins.df), nb.rand.bins)),]
   }
@@ -21,49 +22,52 @@ qc.samples.cluster <- function(bc.df, nb.rand.bins=NULL){
   pc.df$sample = samples
   cat("Done.\n")
 
-  PC1 = PC2 = x= y = xend = yend = sample = NULL ## Uglily appease R checks
-  shiny::runApp(list(
+  if(shiny.app){
+      PC1 = PC2 = x= y = xend = yend = sample = NULL ## Uglily appease R checks
+      shiny::runApp(list(
 
-      ui = shiny::fluidPage(
-          shiny::titlePanel("PopSV - QC sample clustering"),
-          shiny::sidebarPanel(
-              shiny::numericInput("nb.clust", "Number of clusters",1, 1, 10, 1),
-              shiny::selectInput("cl.meth", "Clustering method", c("complete","average","ward")),
-              shiny::numericInput("cl.sel", "Cluster selected",1, 1, 10, 1),
-              shiny::hr(),
-              shiny::actionButton("exp","Done"),
-              shiny::hr(),
-              shiny::textOutput("export")
+          ui = shiny::fluidPage(
+              shiny::titlePanel("PopSV - QC sample clustering"),
+              shiny::sidebarPanel(
+                  shiny::numericInput("nb.clust", "Number of clusters",1, 1, 10, 1),
+                  shiny::selectInput("cl.meth", "Clustering method", c("complete","average","ward")),
+                  shiny::numericInput("cl.sel", "Cluster selected",1, 1, 10, 1),
+                  shiny::hr(),
+                  shiny::actionButton("exp","Done"),
+                  shiny::hr(),
+                  shiny::textOutput("export")
+                  ),
+              shiny::mainPanel(shiny::plotOutput("pca", height=800))
               ),
-          shiny::mainPanel(shiny::plotOutput("pca", height=800))
-          ),
 
-      server = function(input, output) {
-        samples.ref <- shiny::reactive({
-            d.pca = stats::dist(pc.df[, c("PC1","PC2")])
-            hc.o = stats::hclust(d.pca, method=input$cl.meth)
-            samp.cl = stats::cutree(hc.o, input$nb.clust)
-            data.frame(pc.df,
-                       cluster=samp.cl,
-                       selected = samp.cl == input$cl.sel)
-          })
-        output$pca = shiny::renderPlot({
-            samp.df = samples.ref()
-            pdf = merge(pc.df, samp.df)
-            pdf$cluster = factor(pdf$cluster)
+          server = function(input, output) {
+              samples.ref <- shiny::reactive({
+                  d.pca = stats::dist(pc.df[, c("PC1","PC2")])
+                  hc.o = stats::hclust(d.pca, method=input$cl.meth)
+                  samp.cl = stats::cutree(hc.o, input$nb.clust)
+                  data.frame(pc.df,
+                             cluster=samp.cl,
+                             selected = samp.cl == input$cl.sel)
+              })
+              output$pca = shiny::renderPlot({
+                  samp.df = samples.ref()
+                  pdf = merge(pc.df, samp.df)
+                  pdf$cluster = factor(pdf$cluster)
 
-            cluster = selected = NULL ## Uglily appeases R checks
-            ggplot2::ggplot(pdf, ggplot2::aes(x=PC1, y=PC2, colour=cluster, size=selected)) +
-                ggplot2::geom_point(alpha=.6) + ggplot2::theme_bw() +
-                    ggplot2::scale_size_manual(values=2:3)
-          })
-        output$export = shiny::renderText({
-            samp.df = shiny::isolate(samples.ref())
-            if(input$exp){
-              shiny::stopApp(samp.df)
-            }
-            return(paste(sum(samp.df$selected),"samples selected."))
-          })
-      }))
-
+                  cluster = selected = NULL ## Uglily appeases R checks
+                  ggplot2::ggplot(pdf, ggplot2::aes(x=PC1, y=PC2, colour=cluster, size=selected)) +
+                      ggplot2::geom_point(alpha=.6) + ggplot2::theme_bw() +
+                          ggplot2::scale_size_manual(values=2:3)
+              })
+              output$export = shiny::renderText({
+                  samp.df = shiny::isolate(samples.ref())
+                  if(input$exp){
+                      shiny::stopApp(samp.df)
+                  }
+                  return(paste(sum(samp.df$selected),"samples selected."))
+              })
+          }))
+  } else {
+      return(pc.df)
+  }
 }
--- R/write.split.samples.R
+++ R/write.split.samples.R
@@ -12,60 +12,61 @@
 ##' @author Jean Monlong
 ##' @export
 write.split.samples <- function(res, files.df, samples=NULL, files.col = c("z","fc"), compress.index = TRUE, nb.cores=1, append=FALSE, reorder=FALSE) {
-  if (length(res) != length(files.col)) {
-    stop("'res' and 'files.col' have different length.")
-  }
-  if(length(res[[1]])==1 & is.character(res[[1]])){
+    if (length(res) != length(files.col)) {
+        stop("'res' and 'files.col' have different length.")
+    }
+    chunk.size = 1e4
+    if(length(res[[1]])==1 & is.character(res[[1]])){
 
-      con.l = lapply(res, file, "r")
-      header.l = lapply(con.l, function(con){
-                            unlist(strsplit(readLines(con, n = 1), "\t"))
-                        })
-      if(is.null(samples)){
-          samples = setdiff(header.l[[1]], c("chr","start","end"))
-      }
+        con.l = lapply(res, file, "r")
+        header.l = lapply(con.l, function(con){
+                              unlist(strsplit(readLines(con, n = 1), "\t"))
+                          })
+        if(is.null(samples)){
+            samples = setdiff(header.l[[1]], c("chr","start","end"))
+        }
 
-      read.chunk <- function(){
-          res.l = lapply(1:length(con.l), function(con.ii){
-                             res = tryCatch(read.table(con.l[[con.ii]], as.is=TRUE, nrows=chunk.size), error=function(e)return(NULL))
-                             if(is.null(res)){
-                                 return(NULL)
-                             }
-                             colnames(res) = header.l[[con.ii]]
-                             col.ii = which(colnames(res) %in% c("chr","start","end",samples))
-                             res[,col.ii]
-                         })
-          if(is.null(res.l[[1]])){
-              return(NULL)
-          }
-          return(res.l)
-      }
+        read.chunk <- function(){
+            res.l = lapply(1:length(con.l), function(con.ii){
+                               res = tryCatch(read.table(con.l[[con.ii]], as.is=TRUE, nrows=chunk.size), error=function(e)return(NULL))
+                               if(is.null(res)){
+                                   return(NULL)
+                               }
+                               colnames(res) = header.l[[con.ii]]
+                               col.ii = which(colnames(res) %in% c("chr","start","end",samples))
+                               res[,col.ii]
+                           })
+            if(is.null(res.l[[1]])){
+                return(NULL)
+            }
+            return(res.l)
+        }
 
-      firstChunk = TRUE
-      while(!is.null((chunk.o = read.chunk()))){
-          write.split.samples(chunk.o, files.df, samples=samples, files.col=files.col, compress.index=FALSE, append=append | !firstChunk)
-          firstChunk = FALSE
-      }
+        firstChunk = TRUE
+        while(!is.null((chunk.o = read.chunk()))){
+            write.split.samples(chunk.o, files.df, samples=samples, files.col=files.col, compress.index=FALSE, append=append | !firstChunk)
+            firstChunk = FALSE
+        }
 
-      close.con = lapply(con.l, close)
-  } else {
-      if(is.null(samples)){
-          samples = setdiff(colnames(res[[1]]), c("chr","start","end"))
-      }
-      tmp = parallel::mclapply(samples, function(samp) {
-                                   for (ii in 1:length(files.col)) {
-                                       res.f = res[[ii]][, c("chr", "start", "end", samp)]
-                                       colnames(res.f)[4] = files.col[ii]
-                                       res.f = with(res.f, dplyr::arrange(res.f, chr, start))
-                                       utils::write.table(res.f, file = files.df[which(files.df$sample == samp), files.col[ii]], row.names = FALSE, quote = FALSE, sep = "\t", append=append, col.names=!append)
-                                   }
-                               }, mc.cores=nb.cores)
-  }
+        close.con = lapply(con.l, close)
+    } else {
+        if(is.null(samples)){
+            samples = setdiff(colnames(res[[1]]), c("chr","start","end"))
+        }
+        tmp = parallel::mclapply(samples, function(samp) {
+                                     for (ii in 1:length(files.col)) {
+                                         res.f = res[[ii]][, c("chr", "start", "end", samp)]
+                                         colnames(res.f)[4] = files.col[ii]
+                                         res.f = with(res.f, dplyr::arrange(res.f, chr, start))
+                                         utils::write.table(res.f, file = files.df[which(files.df$sample == samp), files.col[ii]], row.names = FALSE, quote = FALSE, sep = "\t", append=append, col.names=!append)
+                                     }
+                                 }, mc.cores=nb.cores)
+    }
 
-  if (compress.index) {
-      files.tc = as.character(unlist(files.df[which(files.df$sample %in% samples), files.col]))
-      comp.index.files(files.tc, reorder=reorder)
-  }
+    if (compress.index) {
+        files.tc = as.character(unlist(files.df[which(files.df$sample %in% samples), files.col]))
+        comp.index.files(files.tc, reorder=reorder)
+    }
 
-  return("Done")
+    return("Done")
 }
