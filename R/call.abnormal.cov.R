50ea021cdcf1459bb4f93142a3b4f58580d35f40
Jean Monlong <jean.monlong@mail.mcgill.ca>
2017-03-19 15:31:24 -0400

    on-the-fly z and calls

--- R/call.abnormal.cov.R
+++ R/call.abnormal.cov.R
@@ -33,159 +33,171 @@
 ##' @export
 call.abnormal.cov <- function(files.df, samp, out.pdf = NULL, FDR.th = 0.05, merge.cons.bins = c("stitch", "zscores", "cbs", "no"), stitch.dist=NULL, z.th = c("sdest", "consbins", "sdest2N"), norm.stats = NULL, min.normal.prop = 0.9, aneu.chrs = NULL, gc.df=NULL, sub.z=NULL, outfile.pv=NULL) {
 
-  z.f = files.df$z[which(files.df$sample==samp)]
-  if(!file.exists(z.f)) z.f = paste0(z.f, ".bgz")
-  res.df = utils::read.table(z.f, header=TRUE, as.is=TRUE)
-  fc.f = files.df$fc[which(files.df$sample==samp)]
-  if(!file.exists(fc.f)) fc.f = paste0(fc.f, ".bgz")
-  fc = utils::read.table(fc.f, header=TRUE, as.is=TRUE)
-  ## Check the order is consistent in both files
-  rand.ii = sample.int(nrow(res.df),100)
-  if(nrow(res.df)!=nrow(fc) | any(res.df$start[rand.ii]!=fc$start[rand.ii]) | any(res.df$chr[rand.ii]!=fc$chr[rand.ii])){
-    stop("Z-score and fold-change (fc) files are not in the same order. Maybe recompute them ?")
-  }
-  ##
-  res.df$fc = fc$fc
-  rm(fc)
-
-  res.df = res.df[which(!is.na(res.df$z)),]
-
-  if (!is.null(norm.stats)) {
-    if (is.character(norm.stats) & length(norm.stats) == 1) {
-      headers = utils::read.table(norm.stats, nrows = 1, as.is = TRUE)
-      colC = rep("NULL", length(headers))
-      colC[1:4] = c("character","integer","integer","numeric")
-      norm.stats = utils::read.table(norm.stats, header = TRUE, colClasses = colC)
-    }
-    colnames(norm.stats)[4] = "mean.cov"
-    res.df = merge(res.df, norm.stats, all.x=TRUE)
-  }
-  if(!is.null(gc.df)){
-    res.df$z = z.norm(res.df, gc.df)
-  }
-
-  ## Remove aneuploid chromosomes
-  if (!is.null(aneu.chrs)) {
-    res.df = res.df[which(!(res.df$chr %in% aneu.chrs)), ]
-  }
-
-  if (!is.null(out.pdf)) {
-    grDevices::pdf(out.pdf, 13, 10)
-  }
-
-  res.df = res.df[which(!is.na(res.df$z) & !is.infinite(res.df$z) & res.df$z!=0),]
-  bin.width = stats::median(round(res.df$end - res.df$start + 1), na.rm=TRUE)
-  ## Pvalue/Qvalue estimation
-  if (all(is.na(res.df$z)))
-    return(NULL)
-  if (z.th[1] == "sdest") {
-    if (min.normal.prop > 0.98) {
-      stop("Maximum value accepted for 'min.normal.prop' is 0.98.")
-    }
-    if(is.null(sub.z)){
-      fdr = fdrtool.quantile(res.df$z, quant.int = seq(min.normal.prop, 0.99, 0.01))
+    if(!is.data.frame(files.df) & is.data.frame(files.df$z)){
+        res.df = files.df$z
+        if(all(samp != colnames(res.df))){
+            stop(samp, " is not in the z-score data.frame.")
+        }
+        res.df = res.df[,c(colnames(res.df)[1:3], samp)]
+        colnames(res.df)[4] = "z"
+        if(is.data.frame(files.df$fc) & samp %in% colnames(files.df$fc)){
+            res.df$fc = files.df$fc[,samp]
+        }
     } else {
-      ## Get 100 sub-segments of 'sub.z' consecutive bins and use the 20 with median closest to 0 (to focus on "normal" genome and avoid large aberrations)
-      res.df = res.df[order(res.df$chr, res.df$start),]
-      subz.l = lapply(round(stats::runif(100,1,nrow(res.df)-sub.z-1)), function(ss) res.df[ss:(ss+sub.z),])
-      subz.med = unlist(lapply(subz.l, function(df) stats::median(df$z, na.rm=TRUE)))
-      subz.z = unlist(lapply(subz.l[order(abs(subz.med))[1:20]], function(df)df$z))
-      fdr = fdrtool.quantile(subz.z, quant.int = seq(min.normal.prop, 0.99, 0.01))
-    }
-  } else if (z.th[1] == "consbins") {
-    fdr = z.thres.cons.bins(res.df)
-  } else if (z.th[1] == "sdest2N") {
-    fdr = fdrtool.quantile.2N(res.df$z)
-  } else {
-    stop("'z.th=': available thresholding approaches are : 'stitch', 'zscores'.")
-  }
-
-  res.df$qv = res.df$pv = NA
-  res.df$pv[which(res.df$z > 0)] = 2 * stats::pnorm(-abs(res.df$z[which(res.df$z > 0)]), 0, fdr$sigma.est.dup)
-  res.df$pv[which(res.df$z < 0)] = 2 * stats::pnorm(-abs(res.df$z[which(res.df$z < 0)]), 0, fdr$sigma.est.del)
-  if (any(res.df$pv == 0, na.rm = TRUE)){
-    res.df$pv[which(res.df$pv == 0)] = .Machine$double.xmin
-  }
-  res.df$qv = stats::p.adjust(res.df$pv, method = "fdr")
-
-  if(!is.null(outfile.pv)){
-      pv.df = res.df[,c("chr","start","end","pv","qv")]
-      pv.df = pv.df[order(pv.df$chr, pv.df$start),]
-      utils::write.table(pv.df, outfile.pv, quote=FALSE, row.names=FALSE, sep="\t")
-      comp.index.files(outfile.pv)
-      rm(pv.df)
-  }
-
-  if (!is.null(out.pdf) & any(!is.na(res.df$pv))) {
-    pv = qv = ..density.. = y = z = NULL  ## Uglily appease R checks
-
-    z.lim = c(-fdr$sigma.est.del, fdr$sigma.est.dup)*ifelse(mean(res.df$pv<.01)>.1,8,5)
-    null.df = data.frame(y=c(stats::dnorm(seq(z.lim[1],0,.05),0,fdr$sigma.est.del),stats::dnorm(seq(0,z.lim[2],.05),0,fdr$sigma.est.dup)), z=c(seq(z.lim[1],0,.05),seq(0,z.lim[2],.05)))
-    null.df$y = null.df$y * mean(res.df$z> -4*fdr$sigma.est.del & res.df$z<4*fdr$sigma.est.dup)
-
-    print(ggplot2::ggplot(res.df, ggplot2::aes(x = z)) +
-          ggplot2::geom_histogram(ggplot2::aes(y=..density..), bins=30, na.rm=TRUE) +
-          ggplot2::xlab("Z-score") + ggplot2::ylab("number of bins") + ggplot2::theme_bw() +
-          ggplot2::geom_line(ggplot2::aes(y=y), data=null.df, linetype=2, colour="red") +
-          ggplot2::xlim(z.lim))
-
-    print(ggplot2::ggplot(res.df, ggplot2::aes(x = pv, fill=cut(qv, breaks = c(-Inf, 0.001, 0.01, 0.5, 0.1,1)))) + ggplot2::geom_histogram(bins=30, na.rm=TRUE) +
-          ggplot2::xlab("P-value") + ggplot2::xlim(-0.2, 1) + ggplot2::ylab("number of bins") +
-          ggplot2::scale_fill_hue(name="Q-value") +
-          ggplot2::theme_bw() + ggplot2::theme(legend.position="bottom"))
-  }
-
-
-  if (merge.cons.bins[1] != "no") {
-    if(is.null(stitch.dist)) {
-      stitch.dist = bin.width + 1
+        z.f = files.df$z[which(files.df$sample==samp)]
+        if(!file.exists(z.f)) z.f = paste0(z.f, ".bgz")
+        res.df = utils::read.table(z.f, header=TRUE, as.is=TRUE)
+        fc.f = files.df$fc[which(files.df$sample==samp)]
+        if(!file.exists(fc.f)) fc.f = paste0(fc.f, ".bgz")
+        fc = utils::read.table(fc.f, header=TRUE, as.is=TRUE)
+        ## Check the order is consistent in both files
+        rand.ii = sample.int(nrow(res.df),100)
+        if(nrow(res.df)!=nrow(fc) | any(res.df$start[rand.ii]!=fc$start[rand.ii]) | any(res.df$chr[rand.ii]!=fc$chr[rand.ii])){
+            stop("Z-score and fold-change (fc) files are not in the same order. Maybe recompute them ?")
+        }
+        ##
+        res.df$fc = fc$fc
+        rm(fc)
     }
 
-    if (merge.cons.bins[1] == "stitch") {
-      res.df = res.df[which(res.df$qv < FDR.th), ]
-      res.df = mergeConsBin.reduce(res.df, stitch.dist = stitch.dist)
-    } else if (merge.cons.bins[1] == "zscores") {
-      res.df = mergeConsBin.z(res.df, fdr.th = FDR.th, sd.null = max(c(fdr$sigma.est.dup,fdr$sigma.est.del)), stitch.dist = stitch.dist)
-    } else if (merge.cons.bins[1] == "cbs") {
-      res.df = mergeConsBin.cbs(res.df, pv.th = FDR.th, stitch.dist = stitch.dist)
+    res.df = res.df[which(!is.na(res.df$z)),]
+
+    if (!is.null(norm.stats)) {
+        if (is.character(norm.stats) & length(norm.stats) == 1) {
+            headers = utils::read.table(norm.stats, nrows = 1, as.is = TRUE)
+            colC = rep("NULL", length(headers))
+            colC[1:4] = c("character","integer","integer","numeric")
+            norm.stats = utils::read.table(norm.stats, header = TRUE, colClasses = colC)
+        }
+        colnames(norm.stats)[4] = "mean.cov"
+        res.df = merge(res.df, norm.stats, all.x=TRUE)
+    }
+    if(!is.null(gc.df)){
+        res.df$z = z.norm(res.df, gc.df)
+    }
+
+    ## Remove aneuploid chromosomes
+    if (!is.null(aneu.chrs)) {
+        res.df = res.df[which(!(res.df$chr %in% aneu.chrs)), ]
+    }
+
+    if (!is.null(out.pdf)) {
+        grDevices::pdf(out.pdf, 13, 10)
+    }
+
+    res.df = res.df[which(!is.na(res.df$z) & !is.infinite(res.df$z) & res.df$z!=0),]
+    bin.width = stats::median(round(res.df$end - res.df$start + 1), na.rm=TRUE)
+    ## Pvalue/Qvalue estimation
+    if (all(is.na(res.df$z)))
+        return(NULL)
+    if (z.th[1] == "sdest") {
+        if (min.normal.prop > 0.98) {
+            stop("Maximum value accepted for 'min.normal.prop' is 0.98.")
+        }
+        if(is.null(sub.z)){
+            fdr = fdrtool.quantile(res.df$z, quant.int = seq(min.normal.prop, 0.99, 0.01))
+        } else {
+            ## Get 100 sub-segments of 'sub.z' consecutive bins and use the 20 with median closest to 0 (to focus on "normal" genome and avoid large aberrations)
+            res.df = res.df[order(res.df$chr, res.df$start),]
+            subz.l = lapply(round(stats::runif(100,1,nrow(res.df)-sub.z-1)), function(ss) res.df[ss:(ss+sub.z),])
+            subz.med = unlist(lapply(subz.l, function(df) stats::median(df$z, na.rm=TRUE)))
+            subz.z = unlist(lapply(subz.l[order(abs(subz.med))[1:20]], function(df)df$z))
+            fdr = fdrtool.quantile(subz.z, quant.int = seq(min.normal.prop, 0.99, 0.01))
+        }
+    } else if (z.th[1] == "consbins") {
+        fdr = z.thres.cons.bins(res.df)
+    } else if (z.th[1] == "sdest2N") {
+        fdr = fdrtool.quantile.2N(res.df$z)
     } else {
-      stop("'merge.cons.bins=' : available bin merging approaches are : 'stitch', 'zscores'.")
+        stop("'z.th=': available thresholding approaches are : 'stitch', 'zscores'.")
     }
 
-    if (nrow(res.df) > 0 & !is.null(out.pdf)) {
-      nb.bin.cons = NULL  ## Uglily appease R checks
-      print(ggplot2::ggplot(res.df, ggplot2::aes(x = factor(nb.bin.cons))) +
-              ggplot2::geom_bar(na.rm=TRUE) + ggplot2::ylab("number of bins") + ggplot2::xlab("number of consecutive abnormal bins") +
-                ggplot2::theme_bw())
-
-      if (any(colnames(res.df) == "fc") & sum(res.df$nb.bin.cons > 2 & res.df$fc < 2.5) > 3) {
-        print(ggplot2::ggplot(subset(res.df, nb.bin.cons > 2), ggplot2::aes(x = 2 * fc)) + ggplot2::geom_histogram(bins=30, na.rm=TRUE) + ggplot2::theme_bw() + ggplot2::ylab("number of bins") + ggplot2::xlab("copy number estimate") + ggplot2::ggtitle("At least 3 consecutive abnormal bins") + ggplot2::xlim(0, 5))
-      }
+    res.df$qv = res.df$pv = NA
+    res.df$pv[which(res.df$z > 0)] = 2 * stats::pnorm(-abs(res.df$z[which(res.df$z > 0)]), 0, fdr$sigma.est.dup)
+    res.df$pv[which(res.df$z < 0)] = 2 * stats::pnorm(-abs(res.df$z[which(res.df$z < 0)]), 0, fdr$sigma.est.del)
+    if (any(res.df$pv == 0, na.rm = TRUE)){
+        res.df$pv[which(res.df$pv == 0)] = .Machine$double.xmin
     }
-  }
+    res.df$qv = stats::p.adjust(res.df$pv, method = "fdr")
 
-  if (!is.null(out.pdf)) {
-    grDevices::dev.off()
-  }
+    if(!is.null(outfile.pv)){
+        pv.df = res.df[,c("chr","start","end","pv","qv")]
+        pv.df = pv.df[order(pv.df$chr, pv.df$start),]
+        utils::write.table(pv.df, outfile.pv, quote=FALSE, row.names=FALSE, sep="\t")
+        comp.index.files(outfile.pv)
+        rm(pv.df)
+    }
 
-  ## Deviation from copy number 2
-  res.df$cn2.dev = round(2*abs(res.df$fc - 1), 4)
-  res.df$cn = round(2*res.df$fc)
-  if(nrow(res.df)>0){
-    res.df$prop.single.bin = round(mean(res.df$nb.bin.cons==1), 3)
-    res.df$pv = signif(res.df$pv, 4)
-    res.df$qv = signif(res.df$qv, 4)
-    res.df$z = round(res.df$z, 2)
-    res.df$fc = round(res.df$fc, 5)
-  } else {
-    res.df$prop.single.bin = res.df$pv = res.df$qv = res.df$z = res.df$fc = numeric(0)
-  }
+    if (!is.null(out.pdf) & any(!is.na(res.df$pv))) {
+        pv = qv = ..density.. = y = z = NULL  ## Uglily appease R checks
 
-  if (nrow(res.df) > 0 & merge.cons.bins[1] != "no") {
-    return(data.frame(sample = samp, res.df, stringsAsFactors = FALSE))
-  } else if (any(res.df$qv <= FDR.th, na.rm = TRUE)) {
-    return(data.frame(sample = samp, res.df[which(res.df$qv <= FDR.th), ], stringsAsFactors = FALSE))
-  } else {
-    return(data.frame())
-  }
+        z.lim = c(-fdr$sigma.est.del, fdr$sigma.est.dup)*ifelse(mean(res.df$pv<.01)>.1,8,5)
+        null.df = data.frame(y=c(stats::dnorm(seq(z.lim[1],0,.05),0,fdr$sigma.est.del),stats::dnorm(seq(0,z.lim[2],.05),0,fdr$sigma.est.dup)), z=c(seq(z.lim[1],0,.05),seq(0,z.lim[2],.05)))
+        null.df$y = null.df$y * mean(res.df$z> -4*fdr$sigma.est.del & res.df$z<4*fdr$sigma.est.dup)
+
+        print(ggplot2::ggplot(res.df, ggplot2::aes(x = z)) +
+                  ggplot2::geom_histogram(ggplot2::aes(y=..density..), bins=30, na.rm=TRUE) +
+                      ggplot2::xlab("Z-score") + ggplot2::ylab("number of bins") + ggplot2::theme_bw() +
+                          ggplot2::geom_line(ggplot2::aes(y=y), data=null.df, linetype=2, colour="red") +
+                              ggplot2::xlim(z.lim))
+
+        print(ggplot2::ggplot(res.df, ggplot2::aes(x = pv, fill=cut(qv, breaks = c(-Inf, 0.001, 0.01, 0.5, 0.1,1)))) + ggplot2::geom_histogram(bins=30, na.rm=TRUE) +
+                  ggplot2::xlab("P-value") + ggplot2::xlim(-0.2, 1) + ggplot2::ylab("number of bins") +
+                      ggplot2::scale_fill_hue(name="Q-value") +
+                          ggplot2::theme_bw() + ggplot2::theme(legend.position="bottom"))
+    }
+
+
+    if (merge.cons.bins[1] != "no") {
+        if(is.null(stitch.dist)) {
+            stitch.dist = bin.width + 1
+        }
+
+        if (merge.cons.bins[1] == "stitch") {
+            res.df = res.df[which(res.df$qv < FDR.th), ]
+            res.df = mergeConsBin.reduce(res.df, stitch.dist = stitch.dist)
+        } else if (merge.cons.bins[1] == "zscores") {
+            res.df = mergeConsBin.z(res.df, fdr.th = FDR.th, sd.null = max(c(fdr$sigma.est.dup,fdr$sigma.est.del)), stitch.dist = stitch.dist)
+        } else if (merge.cons.bins[1] == "cbs") {
+            res.df = mergeConsBin.cbs(res.df, pv.th = FDR.th, stitch.dist = stitch.dist)
+        } else {
+            stop("'merge.cons.bins=' : available bin merging approaches are : 'stitch', 'zscores'.")
+        }
+
+        if (nrow(res.df) > 0 & !is.null(out.pdf)) {
+            nb.bin.cons = NULL  ## Uglily appease R checks
+            print(ggplot2::ggplot(res.df, ggplot2::aes(x = factor(nb.bin.cons))) +
+                      ggplot2::geom_bar(na.rm=TRUE) + ggplot2::ylab("number of bins") + ggplot2::xlab("number of consecutive abnormal bins") +
+                          ggplot2::theme_bw())
+
+            if (any(colnames(res.df) == "fc") & sum(res.df$nb.bin.cons > 2 & res.df$fc < 2.5) > 3) {
+                print(ggplot2::ggplot(subset(res.df, nb.bin.cons > 2), ggplot2::aes(x = 2 * fc)) + ggplot2::geom_histogram(bins=30, na.rm=TRUE) + ggplot2::theme_bw() + ggplot2::ylab("number of bins") + ggplot2::xlab("copy number estimate") + ggplot2::ggtitle("At least 3 consecutive abnormal bins") + ggplot2::xlim(0, 5))
+            }
+        }
+    }
+
+    if (!is.null(out.pdf)) {
+        grDevices::dev.off()
+    }
+
+    ## Deviation from copy number 2
+    res.df$cn2.dev = round(2*abs(res.df$fc - 1), 4)
+    res.df$cn = round(2*res.df$fc)
+    if(nrow(res.df)>0){
+        res.df$prop.single.bin = round(mean(res.df$nb.bin.cons==1), 3)
+        res.df$pv = signif(res.df$pv, 4)
+        res.df$qv = signif(res.df$qv, 4)
+        res.df$z = round(res.df$z, 2)
+        res.df$fc = round(res.df$fc, 5)
+    } else {
+        res.df$prop.single.bin = res.df$pv = res.df$qv = res.df$z = res.df$fc = numeric(0)
+    }
+
+    if (nrow(res.df) > 0 & merge.cons.bins[1] != "no") {
+        return(data.frame(sample = samp, res.df, stringsAsFactors = FALSE))
+    } else if (any(res.df$qv <= FDR.th, na.rm = TRUE)) {
+        return(data.frame(sample = samp, res.df[which(res.df$qv <= FDR.th), ], stringsAsFactors = FALSE))
+    } else {
+        return(data.frame())
+    }
 }
--- R/z.comp.R
+++ R/z.comp.R
@@ -17,98 +17,113 @@
 ##' @param append should the Z-scores be appended to existing files. Default is FALSE.
 ##' @param recomp.msd Should the mean/SD be recomputed and written to 'norm.stats.f' ? Default is FALSE. Most of the times you don't want that but it's useful when combining discordant mapping.
 ##' @return a list with
-##' \item{samples}{a vector with the names of the samples analyzed.}
+##' \item{z}{a data.frame with the Z-scores if 'bc.f' was a data.frame. NULL otherwise.}
+##' \item{fc}{a data.frame with the fold-changes if 'bc.f' was a data.frame. NULL otherwise.}
+##' \item{msd}{a data.frame with the mean-sd information if 'bc.f' was a data.frame. NULL otherwise.}
 ##' \item{z.poisson}{was Normal-Poisson hybrid Z-score score computed.}
 ##' @author Jean Monlong
 ##' @export
 z.comp <- function(bc.f, norm.stats.f, files.df, z.poisson = FALSE, nb.cores = 1, chunk.size=1e4, append=FALSE, recomp.msd=FALSE) {
 
-  if(!file.exists(bc.f)){
-    stop("Bin count file not found.")
-  }
-
-  if (z.poisson) {
-    z.comp.f <- function(x, mean.c, sd.c) {
-      z.n = (x - mean.c)/sd.c
-      z.p = stats::qnorm(stats::ppois(x, mean.c))
-      n.ii = abs(z.n) < abs(z.p)
-      z.p[which(n.ii)] = z.n[which(n.ii)]
-      z.p
-    }
-  } else {
-    z.comp.f <- function(x, mean.c, sd.c) {
-      (x - mean.c)/sd.c
-    }
-  }
-
-  ## Open file connection and prepare which columns to read
-  con.bc = file(bc.f, "r")
-  bc.header = unlist(strsplit(readLines(con.bc, n = 1), "\t"))
-  bc.colClasses = c("character", rep("numeric",2), ifelse(bc.header[-(1:3)] %in% files.df$sample, "numeric", "NULL"))
-  bc.header = bc.header[which(bc.colClasses != "NULL")]
-  files.df = files.df[which(files.df$sample %in% bc.header),]
-  if(!recomp.msd){
-    con.ns = file(norm.stats.f, "r")
-    ns.header = unlist(strsplit(readLines(con.ns, n = 1), "\t"))
-    ns.colClasses = c("character", rep("numeric",2), ifelse(ns.header[-(1:3)] %in% c("m","sd"), "numeric", "NULL"))
-    ns.header = ns.header[1:5]
-  }
-
-  read.chunk <- function(){
-      bc.res = tryCatch(utils::read.table(con.bc, colClasses=bc.colClasses, nrows=chunk.size), error=function(e)return(NULL))
-      if(is.null(bc.res)){
-          return(NULL)
-      }
-      colnames(bc.res) = bc.header
-      if(!recomp.msd){
-        ns.res = tryCatch(utils::read.table(con.ns, colClasses=ns.colClasses, nrows=chunk.size), error=function(e)return(NULL))
-        if(is.null(ns.res)){
-          stop("Different number of rows between ", bc.f," and ", norm.stats.f, " !?")
+    if (z.poisson) {
+        z.comp.f <- function(x, mean.c, sd.c) {
+            z.n = (x - mean.c)/sd.c
+            z.p = stats::qnorm(stats::ppois(x, mean.c))
+            n.ii = abs(z.n) < abs(z.p)
+            z.p[which(n.ii)] = z.n[which(n.ii)]
+            z.p
         }
-        colnames(ns.res) = ns.header
-      } else {
-        ns.res = NULL
-      }
-      list(bc=bc.res, ns=ns.res)
-  }
-
-  ## For each chunk until it's NULL
-  firstChunk = TRUE
-  while(!is.null((chunk.o = read.chunk()))){
-
-      ## Read chunk
-      bc.l = chunk.o$bc
-      bc.1 = bc.l[, 1:3]
-    bc.l = as.matrix(bc.l[, -(1:3)])
-
-    if(recomp.msd){
-      msd = parallel::mclapply(1:nrow(bc.l), function(rr) unlist(mean.sd.outlierR(bc.l[rr,])), mc.cores=nb.cores)
-      msd = matrix(unlist(msd), nrow=3)
-      rownames(msd) = c("m","sd","nb.remove")
-      msd = cbind(bc.1, msd)
-      utils::write.table(msd, file=norm.stats.f, row.names=FALSE, sep="\t", col.names=firstChunk, append=!firstChunk)
     } else {
-      msd = chunk.o$ns
+        z.comp.f <- function(x, mean.c, sd.c) {
+            (x - mean.c)/sd.c
+        }
     }
 
-      z = parallel::mclapply(1:ncol(bc.l), function(cc) z.comp.f(bc.l[,cc], mean.c = msd$m, sd.c = msd$sd), mc.cores=nb.cores)
-      z = matrix(unlist(z), ncol=length(z))
-      fc = bc.l/msd$m
-      colnames(z) = colnames(fc) = colnames(bc.l)
-      z = data.frame(bc.1, z, stringsAsFactors=TRUE)
-      fc = data.frame(bc.1, fc, stringsAsFactors=TRUE)
+    if(is.data.frame(bc.f)){
+        read.chunk <- function(){
+            if(!firstChunk) return(NULL)
+            list(bc=bc.f, ns=norm.stats.f)
+        }
+    } else {
+        if(!file.exists(bc.f)){
+            stop("Bin count file not found.")
+        }
 
-      ## Write output files
-      write.split.samples(list(z=z, fc=fc), files.df, files.col=c("z","fc"), compress.index=FALSE, append=append | !firstChunk)
-      firstChunk = FALSE
-  }
+        ## Open file connection and prepare which columns to read
+        con.bc = file(bc.f, "r")
+        bc.header = unlist(strsplit(readLines(con.bc, n = 1), "\t"))
+        bc.colClasses = c("character", rep("numeric",2), ifelse(bc.header[-(1:3)] %in% files.df$sample, "numeric", "NULL"))
+        bc.header = bc.header[which(bc.colClasses != "NULL")]
+        files.df = files.df[which(files.df$sample %in% bc.header),]
+        if(!recomp.msd){
+            con.ns = file(norm.stats.f, "r")
+            ns.header = unlist(strsplit(readLines(con.ns, n = 1), "\t"))
+            ns.colClasses = c("character", rep("numeric",2), ifelse(ns.header[-(1:3)] %in% c("m","sd"), "numeric", "NULL"))
+            ns.header = ns.header[1:5]
+        }
 
-  close(con.bc)
-  if(!recomp.msd){
-    close(con.ns)
-  }
+        read.chunk <- function(){
+            bc.res = tryCatch(utils::read.table(con.bc, colClasses=bc.colClasses, nrows=chunk.size), error=function(e)return(NULL))
+            if(is.null(bc.res)){
+                return(NULL)
+            }
+            colnames(bc.res) = bc.header
+            if(!recomp.msd){
+                ns.res = tryCatch(utils::read.table(con.ns, colClasses=ns.colClasses, nrows=chunk.size), error=function(e)return(NULL))
+                if(is.null(ns.res)){
+                    stop("Different number of rows between ", bc.f," and ", norm.stats.f, " !?")
+                }
+                colnames(ns.res) = ns.header
+            } else {
+                ns.res = NULL
+            }
+            list(bc=bc.res, ns=ns.res)
+        }
+    }
 
-  comp.index.files(c(files.df$z, files.df$fc), rm.input=TRUE, reorder=TRUE)
+    ## For each chunk until it's NULL
+    firstChunk = TRUE
+    while(!is.null((chunk.o = read.chunk()))){
 
-  return(list(z.poisson = z.poisson))
+        ## Read chunk
+        bc.l = chunk.o$bc
+        bc.1 = bc.l[, 1:3]
+        bc.l = as.matrix(bc.l[, -(1:3)])
+
+        if(recomp.msd){
+            msd = parallel::mclapply(1:nrow(bc.l), function(rr) unlist(mean.sd.outlierR(bc.l[rr,])), mc.cores=nb.cores)
+            msd = matrix(unlist(msd), nrow=3)
+            rownames(msd) = c("m","sd","nb.remove")
+            msd = cbind(bc.1, msd)
+            if(!is.data.frame(bc.f)){
+                utils::write.table(msd, file=norm.stats.f, row.names=FALSE, sep="\t", col.names=firstChunk, append=!firstChunk)
+            }
+        } else {
+            msd = chunk.o$ns
+        }
+
+        z = parallel::mclapply(1:ncol(bc.l), function(cc) z.comp.f(bc.l[,cc], mean.c = msd$m, sd.c = msd$sd), mc.cores=nb.cores)
+        z = matrix(unlist(z), ncol=length(z))
+        fc = bc.l/msd$m
+        colnames(z) = colnames(fc) = colnames(bc.l)
+        z = data.frame(bc.1, z, stringsAsFactors=TRUE)
+        fc = data.frame(bc.1, fc, stringsAsFactors=TRUE)
+
+        ## Write output files
+        if(!is.data.frame(bc.f)){
+            write.split.samples(list(z=z, fc=fc), files.df, files.col=c("z","fc"), compress.index=FALSE, append=append | !firstChunk)
+            z = fc = msd = NULL
+        }
+        firstChunk = FALSE
+    }
+
+    if(!is.data.frame(bc.f)){
+        close(con.bc)
+        if(!recomp.msd){
+            close(con.ns)
+        }
+        comp.index.files(c(files.df$z, files.df$fc), rm.input=TRUE, reorder=TRUE)
+    }
+
+    return(list(z=z, fc=fc, msd=msd, z.poisson = z.poisson))
 }
