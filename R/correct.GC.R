a827c9255835b2abbedf6b1af0eea3390405beb1
Jean Monlong <jean.monlong@mail.mcgill.ca>
2017-06-16 22:22:42 +0100

    catch order potential difference

--- R/correct.GC.R
+++ R/correct.GC.R
@@ -6,9 +6,9 @@
 ##' a data.frame with 4 columns: 'chr', 'start', 'end' and 'bc'.
 ##' @param gc.df a data.frame with the bin definition and GC content information.
 ##' Column 'GCcontent' is required and can be obtained thanks to 'getGC.hg19' for
-##' example. 
+##' example.
 ##' @param outfile.prefix the prefix for the output file name. The suffix '.bgz' will
-##' be appended to this name prefix after compression. 
+##' be appended to this name prefix after compression.
 ##' @param appendIndex.outfile if TRUE (default), the results will be saved on the
 ##' output file which will be ultimately compressed/indexed. This is recommend when a large number
 ##' of bins are analyzed. If FALSE, a data.frame with the corrected bin counts will
@@ -19,19 +19,27 @@
 ##' @export
 correct.GC <- function(bc.f, gc.df, outfile.prefix, appendIndex.outfile = TRUE) {
     gc.class = cut(gc.df$GCcontent, breaks = seq(0, 1, 0.02), include.lowest = TRUE)
-    samp.ii = unlist(tapply(1:length(gc.class), gc.class, function(e) e[sample(1:length(e), 
+    samp.ii = unlist(tapply(1:length(gc.class), gc.class, function(e) e[sample(1:length(e),
         min(c(length(e), 500)))]))
-    bc.df = utils::read.table(bc.f, as.is = TRUE, header = TRUE)
-    bc.df$GCcontent = gc.df$GCcontent
+    if(!as.data.frame(bc.f) & length(bc.f)==1 & is.character(bc.f)){
+      bc.df = utils::read.table(bc.f, as.is = TRUE, header = TRUE)
+    } else {
+      bc.df = bc.df
+    }
+    if(any(bc.df$chr != gc.df$chr) | any(bc.df$start != gc.df$start)){
+      bc.df = merge(bc.df, gc.df[,c('chr','start','end','GCcontent')])
+    } else {
+      bc.df$GCcontent = gc.df$GCcontent
+    }
     lo = stats::loess(bc ~ GCcontent, data = bc.df[samp.ii, ])
     bc.df$bc = mean(bc.df$bc, na.rm = TRUE) * bc.df$bc/stats::predict(lo, newdata = bc.df)
     bc.df$bc = round(bc.df$bc, digits = 2)
-    if (any(bc.df$bc < 0, na.rm = TRUE)) 
+    if (any(bc.df$bc < 0, na.rm = TRUE))
         bc.df$bc[bc.df$bc < 0] = 0
     bc.df$GCcontent = NULL
-    
+
     if (appendIndex.outfile) {
-        utils::write.table(bc.df, file = outfile.prefix, quote = FALSE, row.names = FALSE, 
+        utils::write.table(bc.df, file = outfile.prefix, quote = FALSE, row.names = FALSE,
             sep = "\t")
         final.file = paste(outfile.prefix, ".bgz", sep = "")
         Rsamtools::bgzip(outfile.prefix, dest = final.file, overwrite = TRUE)
@@ -41,5 +49,5 @@ correct.GC <- function(bc.f, gc.df, outfile.prefix, appendIndex.outfile = TRUE) 
     } else {
         return(bc.df)
     }
-    
-} 
+
+}
