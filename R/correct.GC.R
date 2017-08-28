6c54f9491e7fc666e658f4a1990a31bf3effb90d
Jean Monlong <jean.monlong@mail.mcgill.ca>
2017-08-28 01:25:31 -0400

    Order after merge to avoid indexing error [ci skip]

--- R/correct.GC.R
+++ R/correct.GC.R
@@ -28,6 +28,7 @@ correct.GC <- function(bc.f, gc.df, outfile.prefix, appendIndex.outfile = TRUE) 
     }
     if(any(bc.df$chr != gc.df$chr) | any(bc.df$start != gc.df$start)){
       bc.df = merge(bc.df, gc.df[,c('chr','start','end','GCcontent')])
+      bc.df = bc.df[order(as.character(bc.df$chr), bc.df$start),]
     } else {
       bc.df$GCcontent = gc.df$GCcontent
     }
